48a4fb723f92603803568ebdd1f0eb01688f5172
Kris Geusebroek <krisgeus@gmail.com>
2014-05-10 00:33:50 +0200

    Third plot

--- plot3.R
+++ plot3.R
@@ -8,14 +8,9 @@ data_one_and_two_feb_2007 <- subset(data, Date == "1/2/2007" | Date == "2/2/2007
 data_one_and_two_feb_2007$DateTime <- as.POSIXct(paste(data_one_and_two_feb_2007$Date, data_one_and_two_feb_2007$Time), format="%d/%m/%Y %H:%M:%S", tz="UTC")
 head(data_one_and_two_feb_2007)
 # the plot
-quartz.options(width=480, height=480)
 plot(Sub_metering_1 ~ DateTime, data=data_one_and_two_feb_2007, ylab = "Energy sub metering", xlab="", type="l")
-#lines(Sub_metering_1 ~ DateTime, data=data_one_and_two_feb_2007, lwd=1)
 lines(Sub_metering_2 ~ DateTime, data=data_one_and_two_feb_2007, lwd=1, col="red")
 lines(Sub_metering_3 ~ DateTime, data=data_one_and_two_feb_2007, lwd=1, col="blue")
 legend(x="topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lwd=1, text.width=strwidth("                 Sub_metering_1"))
 dev.copy(png, filename="ExData_Plotting1/plot3.png", width=480, height=480)
 dev.off()
-
-strwidth("Sub_metering_1") + 1000
-?legend
